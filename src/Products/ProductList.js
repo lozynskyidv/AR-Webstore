0c251cff1ffe23968d813f6428f9d628f09ff558
Dmytro Lozynskyi <lozynskyidv@gmail.com>
2023-08-06 13:17:55 +0100

    Aquarium only

--- src/Products/ProductList.js
+++ src/Products/ProductList.js
@@ -1,7 +1,5 @@
 import sheenchair from "../assets/models/aquarium.glb";
 import ioschair from "../assets/models/aquarium.usdz";
-import car from "../assets/models/cat-tree.glb";
-import ioscar from "../assets/models/cat-tree.usdz";
 
 
 const items = [
@@ -12,14 +10,6 @@ const items = [
     iosSrc: ioschair,
     category: "Fish",
     color: "Brown",
-},
-{
-    id: 2,
-    name: "Cat Tree",
-    src: car,
-    iosSrc: ioscar,
-    category: "Pet Stuff",
-    color: "White",
 },
 
 ];
