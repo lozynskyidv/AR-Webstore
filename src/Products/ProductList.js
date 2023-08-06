4bab1c9ce9d3b70a2f490805e0f83dc55822097d
Dmytro Lozynskyi <lozynskyidv@gmail.com>
2023-08-06 13:16:16 +0100

    2 products

--- src/Products/ProductList.js
+++ src/Products/ProductList.js
@@ -1,7 +1,5 @@
 import sheenchair from "../assets/models/aquarium.glb";
 import ioschair from "../assets/models/aquarium.usdz";
-import painting from "../assets/models/dog-house.glb";
-import iospainting from "../assets/models/dog-house.usdz";
 import car from "../assets/models/cat-tree.glb";
 import ioscar from "../assets/models/cat-tree.usdz";
 
@@ -17,14 +15,6 @@ const items = [
 },
 {
     id: 2,
-    name: "Dog House",
-    src: painting,
-    iosSrc: iospainting,
-    category: "Pet Houses",
-    color: "Dark",
-},
-{
-    id: 3,
     name: "Cat Tree",
     src: car,
     iosSrc: ioscar,
